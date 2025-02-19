8fc6fcb3ecf4ca9cf3242164b1e6fc69501dd618
aquapau <pm.semenova@yandex.ru>
2025-02-19 07:55:16 +0300

    3.1: countries list update

--- src/components/CreateCompanyModal.jsx
+++ src/components/CreateCompanyModal.jsx
@@ -8,12 +8,8 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 
 const countries = [
   { code: "RU", name: "Russia" },
-  { code: "US", name: "United States" },
-  { code: "GB", name: "United Kingdom" },
-  { code: "DE", name: "Germany" },
-  { code: "FR", name: "France" },
-  { code: "CN", name: "China" },
-  { code: "JP", name: "Japan" },
+  { code: "BY", name: "Belarus" },
+  { code: "KZ", name: "Kazakhstan" }
 ]
 
 const CreateCompanyModal = ({ isOpen, onClose, onCreateCompany }) => {
