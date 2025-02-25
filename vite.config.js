1e93681db389a6bcdbffd0aa7a7e154183264409
aquapau <pm.semenova@yandex.ru>
2025-02-25 23:21:49 +0300

    2: enhancements

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       },
   },
   server: {
-      allowedHosts: [negotiation-app-web-aquapau.amvera.io],
+      allowedHosts: ["negotiation-app-web-aquapau.amvera.io"],
       watch: {
         usePolling: true,
       },
