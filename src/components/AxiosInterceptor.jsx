38fe350c2e6490dcf450afad2ca8c9eb8cc7c278
AquaPau <36632855+AquaPau@users.noreply.github.com>
2025-04-22 00:57:34 +0300

    faq (#39)

--- src/components/AxiosInterceptor.jsx
+++ src/components/AxiosInterceptor.jsx
@@ -3,16 +3,24 @@
 import { useEffect } from "react"
 import { setupAxiosInterceptors } from "../api/api"
 import { useAuth } from "../context/AuthContext"
+import { useLocation } from "react-router-dom"
 
 export const AxiosInterceptor = ({ children }) => {
   const { _, loading, login, logout, handleUnauthorized } = useAuth()
-
-
+  const location = useLocation()
 
   useEffect(() => {
-    setupAxiosInterceptors(handleUnauthorized)
-  }, [handleUnauthorized])
+    const customUnauthorizedHandler = () => {
+      if (location.pathname === "/faq") {
+        return
+      }
+      handleUnauthorized()
+    }
+
+    setupAxiosInterceptors(customUnauthorizedHandler)
+  }, [handleUnauthorized, location.pathname])
 
   return children
 }
+
 
